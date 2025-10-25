5935be074c3365bd0f3146fdccfd3d46bcab5edf
jatozy <infrastructure@jato-engineering.de>
2025-10-25 23:35:23 +0200

    wip

--- src/cube_solver_hardware/controller.rs
+++ src/cube_solver_hardware/controller.rs
@@ -1,23 +1,35 @@
 use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 pub struct Controller {
-    turntable_angle: u16,
-    hardware: super::rasbpi::Rasbpi,
+    tx: mpsc::Sender<ControllerCommand>,
+}
+
+enum ControllerCommand {
+    RotateTurntable90DegreeClockwise,
 }
 
 impl Controller {
     pub fn new() -> Self {
-        Controller {
-            turntable_angle: 0,
-            hardware: super::rasbpi::Rasbpi::new(),
-        }
-    }
-}
+        let (tx, rx) = mpsc::channel();
+        let mut hardware = super::rasbpi::Rasbpi::new();
+        let mut turntable_angle = 0u16;
 
-impl Controller {
-    pub fn rotate_turntable_90_degree_clockwise(&mut self) -> std::sync::mpsc::Receiver<()> {
+        thread::spawn(move || {
+            while let Ok(cmd) = rx.recv() {
+                match cmd {
+                    ControllerCommand::RotateTurntable90DegreeClockwise => {
+                        hardware.rotate_gpio_motor_right();
+                        turntable_angle = (turntable_angle + 90) % 360;
+                    }
+                }
+            }
+        });
+
+        Controller { tx }
+    }
+
+    pub fn rotate_turntable_90_degree_clockwise(&mut self) -> mpsc::Receiver<()> {
         let (tx, rx) = mpsc::channel();
 
         thread::spawn(move || {
