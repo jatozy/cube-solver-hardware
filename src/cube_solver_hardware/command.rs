b7e5bd9365ed6bba0fd00016dde4e5e52f9b4223
jatozy <52920861+jatozy@users.noreply.github.com>
2025-10-26 11:40:26 +0100

    Feature/add rasbpi control (#3)
    * Add controller for the hardware
    
    * start with rasbpi implementation
    
    * wip
    
    * Create result back channel
    
    * As long as there are no sensors connected, the rotation is done with a dedicated time to test, if the lib works in principle.

---
+++ src/cube_solver_hardware/command.rs
@@ -0,0 +1,20 @@
+use std::sync::mpsc;
+
+pub(crate) struct Command {
+    pub result_back_channel: mpsc::Sender<()>,
+    pub command: Commands,
+}
+
+pub(crate) enum Commands {
+    RotateTurntable90DegreeClockwise,
+    RotateTurntable90DegreeCounterClockwise,
+}
+
+impl Command {
+    pub fn new(command: Commands, result_back_channel: mpsc::Sender<()>) -> Self {
+        Command {
+            command,
+            result_back_channel,
+        }
+    }
+}
---
+++ src/cube_solver_hardware/controller.rs
@@ -0,0 +1,64 @@
+use super::command::Command;
+use super::command::Commands;
+use crate::raspbi::hardware::Hardware;
+use std::sync::mpsc;
+use std::thread;
+
+pub struct Controller {
+    command_sender: mpsc::Sender<Command>,
+}
+
+impl Controller {
+    pub fn new() -> Self {
+        let (command_sender, command_receiver) = mpsc::channel::<Command>();
+        let mut hardware = Hardware::new();
+        let mut turntable_angle = 0u16;
+
+        thread::spawn(move || {
+            while let Ok(cmd) = command_receiver.recv() {
+                match cmd.command {
+                    Commands::RotateTurntable90DegreeClockwise => {
+                        hardware.rotate_gpio_motor_right();
+                        std::thread::sleep(std::time::Duration::from_millis(500));
+                        hardware.stop_gpio_motor();
+                        turntable_angle = (turntable_angle + 90) % 360;
+                        cmd.result_back_channel.send(()).unwrap();
+                    }
+                    Commands::RotateTurntable90DegreeCounterClockwise => {
+                        hardware.rotate_gpio_motor_left();
+                        std::thread::sleep(std::time::Duration::from_millis(500));
+                        hardware.stop_gpio_motor();
+                        turntable_angle = (turntable_angle + 270) % 360;
+                        cmd.result_back_channel.send(()).unwrap();
+                    }
+                }
+            }
+        });
+
+        Controller {
+            command_sender: command_sender,
+        }
+    }
+
+    pub fn rotate_turntable_90_degree_clockwise(&mut self) -> mpsc::Receiver<()> {
+        let (result_sender, result_receiver) = mpsc::channel();
+        self.command_sender
+            .send(Command::new(
+                Commands::RotateTurntable90DegreeClockwise,
+                result_sender,
+            ))
+            .unwrap();
+        result_receiver
+    }
+
+    pub fn rotate_turntable_90_degree_counter_clockwise(&mut self) -> mpsc::Receiver<()> {
+        let (result_sender, result_receiver) = mpsc::channel();
+        self.command_sender
+            .send(Command::new(
+                Commands::RotateTurntable90DegreeCounterClockwise,
+                result_sender,
+            ))
+            .unwrap();
+        result_receiver
+    }
+}
